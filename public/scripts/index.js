fdea9ad2347ac696e2c36a2373c076a436cd77e0
Alexey Kovzel <alexey.kovzel@gmail.com>
2023-12-17 18:52:25 +0100

    Improve web design

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,9 @@
 // Update time values every second
-let unixTime = document.querySelector('#unix-time p');
+let unixTime = document.querySelector('#unix-time h1');
 let subdate = document.querySelector('#subdate');
 
 updateTimes();
 setInterval(updateTimes, 1000);
-
-function updateTimes() {
-    unixTime.innerText = getUnixTime();
-    subdate.innerText = getDateValue();
-}
-
-function getUnixTime() {
-    return (Date.now() / 1000) | 0;
-}
-
-function getDateValue() {
-    let options = {
-        day: 'numeric', month: 'short', year: 'numeric',
-        hour: '2-digit', minute: '2-digit', second: '2-digit'
-    };
-    return new Date().toLocaleDateString('en-US', options);
-}
 
 // Initialize "copy" button 
 let copy = document.querySelector('#unix-time .copy');
@@ -37,6 +20,31 @@ dateField.oninput = updateUnixField;
 timeField.oninput = updateUnixField;
 unixField.oninput = updateTimeField;
 
+// Show main page
+let main = document.querySelector('main')
+main.style.opacity = 1;
+
+function updateTimes() {
+    unixTime.innerText = addCommas(getUnixTime());
+    subdate.innerText = getDateValue();
+}
+
+function addCommas(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getUnixTime() {
+    return (Date.now() / 1000) | 0;
+}
+
+function getDateValue() {
+    let options = {
+        day: 'numeric', month: 'short', year: 'numeric',
+        hour: '2-digit', minute: '2-digit', second: '2-digit'
+    };
+    return new Date().toLocaleDateString('en-US', options);
+}
+
 function updateUnixField() {
     let date = new Date(dateField.value + 'T' + timeField.value);
     unixField.value = isNaN(date) ? null : Math.floor(date.getTime() / 1000);
@@ -47,7 +55,3 @@ function updateTimeField() {
     dateField.valueAsDate = date;
     timeField.valueAsDate = date;
 }
-
-// Show main page
-let main = document.querySelector('main')
-main.style.opacity = 1;
